5a8228f59118bc0b878f2c3dfce89c1e8ce44d9a
Evgenii Gusev <ged1994@mail.ru>
2024-10-03 13:23:00 +0300

    added config

---
+++ internal/config/config.go
@@ -0,0 +1,43 @@
+package config
+
+import (
+	"github.com/ilyakaznacheev/cleanenv"
+	"log"
+	"os"
+)
+
+type Config struct {
+	DB Database
+}
+
+type Database struct {
+	DBName   string `yaml:"dbname" env:"DATABASE_NAME"`
+	User     string `yaml:"user" env:"DATABASE_USER"`
+	Password string `yaml:"password" env:"DATABASE_PASSWORD"`
+	Host     string `yaml:"host" env:"DATABASE_HOST"`
+	Port     string `yaml:"port" env:"DATABASE_PORT"`
+}
+
+func MustLoad() *Config {
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		log.Fatalf("CONFIG_PATH environment variable is not set.")
+	}
+
+	return MustLoadByPath(configPath)
+}
+
+func MustLoadByPath(configPath string) *Config {
+	// check if file exists
+	if _, err := os.Stat(configPath); err != nil {
+		log.Fatalf("Config path does not exist: %s", configPath)
+	}
+
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		log.Fatalf("Failed to read config.")
+	}
+
+	return &cfg
+}
